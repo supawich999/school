9ad0e9af24a80914089e12ad6cf3ab67bc3e1adf
Worawut <aof.gang60@gmail.com>
2021-01-05 11:10:44 +0700

    craete model

--- src/models/student.ts
+++ src/models/student.ts
@@ -1,3 +1,10 @@
 export interface student {
-    
+    stuId: string;
+    stuPass: string;
+    stuNameSurname: string;
+    stuAddress: string;
+    stuAge: number;
+    stuPak: string;
+    stuSaka: string;
+    subL: string;
 }
---
+++ src/models/subject.ts
@@ -0,0 +1,7 @@
+export interface subject {
+    subId: string;
+    subName: string;
+    subPak: string;
+    subSaka: string;
+    subStatus: string;
+}
