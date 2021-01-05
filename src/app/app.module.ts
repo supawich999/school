d33114570914fae26b40819f3f2c35c8b0a7e27f
Aekkapoom Tobterb <Bboy4457@gmail.com>
2021-01-05 13:47:42 +0700

    add data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TeacherComponent } from './teacher/teacher.component';
 import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/teacher/teacher.component.ts
+++ src/app/teacher/teacher.component.ts
@@ -1,4 +1,6 @@
+import { teacher } from './../../models/teacher';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-teacher',
@@ -7,9 +9,119 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeacherComponent implements OnInit {
 
+  data01: teacher[] = [
+    {
+      teaId: "T001",
+      teaPass: "P001",
+      teaNameSurname: "Aekkapoom Tobterb",
+      teaAddress: "Mahasarakham",
+      teaAge: 22,
+      teaPak: "Information Technology",
+      teaSaka: "IT",
+      teaL: "SA01",
+    },
+    {
+      teaId: "T002",
+      teaPass: "P002",
+      teaNameSurname: "Nongnapat Sakonvit",
+      teaAddress: "Mukdahan",
+      teaAge: 25,
+      teaPak: "Accounting",
+      teaSaka: "AC",
+      teaL: "SA02",
+    },
+    {
+      teaId: "T003",
+      teaPass: "P003",
+      teaNameSurname: "Wisut Noreerat",
+      teaAddress: "Khonaen",
+      teaAge: 21,
+      teaPak: "Political",
+      teaSaka: "PA",
+      teaL: "SC03",
+    },
+    {
+      teaId: "T004",
+      teaPass: "P004",
+      teaNameSurname: "Rachata Lakronsri",
+      teaAddress: "Mahasarakham",
+      teaAge: 27,
+      teaPak: "Information Technology",
+      teaSaka: "CS",
+      teaL: "SD04",
+    },
+    {
+      teaId: "T005",
+      teaPass: "P005",
+      teaNameSurname: "Thitisak Wichachai",
+      teaAddress: "Mahasarakham",
+      teaAge: 23,
+      teaPak: "Accounting",
+      teaSaka: "GM",
+      teaL: "SE05",
+    },
+    {
+      teaId: "T006",
+      teaPass: "P006",
+      teaNameSurname: "Staporn Longthon",
+      teaAddress: "Sakon Nakhon",
+      teaAge: 29,
+      teaPak: "Agriculture",
+      teaSaka: "AC",
+      teaL: "SF06",
+    },
+    {
+      teaId: "T007",
+      teaPass: "P007",
+      teaNameSurname: "Kitti Jitvigran",
+      teaAddress: "Mahasarakham",
+      teaAge: 35,
+      teaPak: "Information Technology",
+      teaSaka: "MC",
+      teaL: "SG07",
+    },
+    {
+      teaId: "T008",
+      teaPass: "P008",
+      teaNameSurname: "Supakorn Jaikhan",
+      teaAddress: "Chaiyaphum",
+      teaAge: 35,
+      teaPak: "Information Technology",
+      teaSaka: "GIS",
+      teaL: "SH08",
+    },
+    {
+      teaId: "T009",
+      teaPass: "P009",
+      teaNameSurname: "Aphiwat Mingchai",
+      teaAddress: "Nongbualamphu",
+      teaAge: 39,
+      teaPak: "Information Technology",
+      teaSaka: "CMD",
+      teaL: "SI09",
+    },
+    {
+      teaId: "T010",
+      teaPass: "P010",
+      teaNameSurname: "Athirat Ritthising",
+      teaAddress: "Buengkan",
+      teaAge: 35,
+      teaPak: "Information Technology",
+      teaSaka: "IT",
+      teaL: "SJ10",
+    },
+  ]
+
+  public isCollapsed = true;
+
+
   constructor() { }
 
   ngOnInit(): void {
+    function myFunction() {
+    }
   }
 
+}
+export class NgbdDropdownForm {
 }
