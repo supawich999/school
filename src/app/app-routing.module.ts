30634d6cc535bea960031923fa281194fd036b67
Worawut <aof.gang60@gmail.com>
2021-01-05 16:04:59 +0700

    Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SubjectComponent } from './subject/subject.component';
+import { StudentComponent } from './student/student.component';
+import { TeacherComponent } from './teacher/teacher.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'subject', component: SubjectComponent},
+  {path: 'student', component: StudentComponent},
+  {path: 'teacher', component: TeacherComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
