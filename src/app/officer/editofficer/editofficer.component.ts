56124ecaf2877bd6bc47f240bbfe84dfebd4b5e2
nipatpon-21 <75299642+nipatpon-21@users.noreply.github.com>
2021-01-06 17:44:32 +0700

    show officer,cancel button

--- src/app/officer/editofficer/editofficer.component.ts
+++ src/app/officer/editofficer/editofficer.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditofficerComponent implements OnInit {
 
+  submit() {
+    alert("Edit complete")
+  }
+
   constructor() { }
 
   ngOnInit(): void {
