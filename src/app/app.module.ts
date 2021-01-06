a595c5ddabe050c55bc29b3465340d2a8474e03b
Worawut <aof.gang60@gmail.com>
2021-01-06 16:06:49 +0700

    form log in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { MaterialModule } from './material/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,16 +12,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SubjectComponent } from './subject/subject.component';
 import { HomeComponent } from './home/home.component';
-<<<<<<< HEAD
 import { OfficerComponent } from './officer/officer.component';
-=======
-import { EditSubjectComponent } from './edit-subject/edit-subject.component';
-import { AddSubjectComponent } from './add-subject/add-subject.component';
-import { DelSubjectComponent } from './del-subject/del-subject.component';
-import { OfficerComponent } from './officer/officer.component';
-
-
->>>>>>> b5189098636d256af3ec8d07fd4e7ec9b2710c5c
+import { HttpClientModule } from "@angular/common/http";
 
 
 @NgModule({
@@ -31,22 +24,16 @@ import { OfficerComponent } from './officer/officer.component';
     StudentComponent,
     SubjectComponent,
     HomeComponent,
-<<<<<<< HEAD
     OfficerComponent
-=======
-    EditSubjectComponent,
-    AddSubjectComponent,
-    DelSubjectComponent,
-    OfficerComponent,
-
->>>>>>> b5189098636d256af3ec8d07fd4e7ec9b2710c5c
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     NgbModule,
-    MaterialModule
+    FormsModule,
+    MaterialModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { student } from '../../models/student';
+import { teacher } from './../../models/teacher';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  user = null;
+  pass = null;
+  name: any = '';
+  first: any;
+  last: any;
+  test() {
+    console.log(this.name);
+    console.log(this.user);
+    console.log(this.pass);
+  }
+
+  onSubmit(a: NgForm) {
+    console.log(a.value.first);
+    console.log(a.value.last);
+  }
+
   constructor() { }
+
+
+  checkstu(data01: student) {
+    console.log(data01.stuId);
+    console.log(data01.stuPass);
+  }
+
+  checktea(data01: teacher) {
+    console.log(data01.teaId);
+    console.log(data01.teaPass);
+  }
 
   ngOnInit(): void {
   }
