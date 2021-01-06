e820951e662590c6125df6d3cd749e226e3fe4b2
Worawut <aof.gang60@gmail.com>
2021-01-06 23:47:23 +0700

    ...

--- src/app/officer/officer.component.ts
+++ src/app/officer/officer.component.ts
@@ -3,6 +3,7 @@ import { officer } from '../../models/officer';
 import { MatDialog } from '@angular/material/dialog';
 import { AddofficerComponent } from '../officer/addofficer/addofficer.component';
 import { EditofficerComponent } from '../officer/editofficer/editofficer.component';
+import { NgbActiveModal, NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-officer',
@@ -54,18 +55,54 @@ export class OfficerComponent implements OnInit {
     }
   ]
 
-  deletealert(){
+  public isCollapsed = true;
+  public officerdata: officer = {
+    offId: "",
+    offPass: "",
+    offNameSurname: " ",
+    offAddress: "",
+    offRank: "",
+    offDepartment: ""
+  };
+
+  deletealert() {
     alert("Delete complete")
   }
 
-  addofficer(){
+  closeResult = "";
+  open(content: any, d: officer) {
+    this.officerdata = d
+    this.modalService
+      .open(content, { ariaLabelledBy: "modal-basic-title" })
+      .result.then(
+        result => {
+          this.closeResult = `Closed with: ${result}`; //Save
+        },
+        reason => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;   //Close Dialog
+        }
+      );
+  }
+
+  addofficer() {
     this.dialog.open(AddofficerComponent);
   }
-  editofficer(){
+  editofficer() {
     this.dialog.open(EditofficerComponent);
   }
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog,
+    private modalService: NgbModal) { }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return "by pressing ESC";
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return "by clicking on a backdrop";
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 
   ngOnInit(): void {
   }
--- src/models/subject.ts
+++ src/models/subject.ts
@@ -5,3 +5,9 @@ export interface subject {
     subSaka: string;
     subStatus: string;
 }
+
+
+  
+  
+  
+ 
