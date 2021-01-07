2364435bace37fcbe8d4557472361954160991d0
nipatpon-21 <75299642+nipatpon-21@users.noreply.github.com>
2021-01-07 17:26:29 +0700

    add validators minlength

--- src/app/officer/addofficer/addofficer.component.ts
+++ src/app/officer/addofficer/addofficer.component.ts
@@ -13,8 +13,8 @@ export class AddofficerComponent implements OnInit {
   profileForm = this.fb.group({
     nameSurname: ['', Validators.required],
     authenForm: this.fb.group({
-      idOfficer: ['', Validators.required],
-      passOfficer: ['', Validators.required],
+      idOfficer: ['', [Validators.required, Validators.minLength(4)]],
+      passOfficer: ['', [Validators.required, Validators.minLength(6)]],
     }),
     dataForm: this.fb.group({
       address: [''],
--- src/app/officer/editofficer/editofficer.component.ts
+++ src/app/officer/editofficer/editofficer.component.ts
@@ -12,8 +12,8 @@ export class EditofficerComponent implements OnInit {
   profileForm = this.fb.group({
     nameSurname: ['', Validators.required],
     authenForm: this.fb.group({
-      idOfficer: ['', Validators.required],
-      passOfficer: ['', Validators.required],
+      idOfficer: ['', [Validators.required, Validators.minLength(4)]],
+      passOfficer: ['', [Validators.required, Validators.minLength(6)]],
     }),
     dataForm: this.fb.group({
       address: [''],
